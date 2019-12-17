c81ce7d54675d95d256f90bee841bdba72816d29
Ben <mrpagan@hotmail.co.uk>
2019-12-17 19:58:08 +0000

    Addition of name selection
    Working on saved frame view

--- app/src/main/java/com/example/hapticfeedback/MainActivity.java
+++ app/src/main/java/com/example/hapticfeedback/MainActivity.java
@@ -1380,6 +1380,7 @@ public class MainActivity extends AppCompatActivity {
             frameList.add(frameName6.toString());*/
         }
 
+
 //        frameList.add(frameName1.toString());
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, frameList);
